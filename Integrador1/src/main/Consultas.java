f8fc5367228fe6d7d733c02e6f43becb3fdeb655
Noelia Carrizo <noeliacarrizo22@gmail.com>
2023-08-28 10:44:00 -0300

    Se le agregan las consultas que se solicitan en los item 3 y 4 del integrador

--- Integrador1/src/main/Consultas.java
+++ Integrador1/src/main/Consultas.java
@@ -43,4 +43,30 @@ public class Consultas {
 		}
 	}
 
+	/* retorna el producto que mas recaudo */
+	private static void productoQueMasRecaudo(Connection conn) throws SQLException {
+		// TODO Auto-generated method stub
+		String result = "SELECT MAX(fp.cantidad * p.valor) as total,fp.*, p.* FROM `factura_producto` as fp inner join producto as p on fp.idProducto = p.idProducto
+		ORDER BY total desc;";
+		java.sql.PreparedStatement ps = conn.prepareStatement(result);
+		ps.executeUpdate();
+		ps.close();
+		conn.commit();
+	}
+
+	/* imprime una lista de clientes ordenada por el cual se le facturo mas */
+	private static void productoQueMasRecaudo(Connection conn) throws SQLException {
+		// TODO Auto-generated method stub
+		String result = "SELECT (fp.cantidad * p.valor) as total,fp.*, p.* , c.*, f.*
+		FROM `factura_producto` as fp 
+		RIGHT join producto as p on fp.idProducto = p.idProducto 
+		left join factura as f on fp.idFactura = f.idFactura
+		left join cliente as c on f.idCliente = c.idCliente
+		ORDER BY total desc;";
+		java.sql.PreparedStatement ps = conn.prepareStatement(result);
+		ps.executeUpdate();
+		ps.close();
+		conn.commit();
+	}
+
 }
