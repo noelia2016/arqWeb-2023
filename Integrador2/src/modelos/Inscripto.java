89600d5a001421beec73bf33809e6f8b7aedef1d
Diego Rodriguez <dsrrdiego@hotmail.com>
2023-09-13 17:52:31 -0300

    listo todo andando perfecto

--- Integrador2/src/modelos/Inscripto.java
+++ Integrador2/src/modelos/Inscripto.java
@@ -5,32 +5,14 @@ import javax.persistence.*;
 @Entity
 public class Inscripto {
 
-	// @EmbeddedId
-	// private claveCompuestaInscripto ids;
 	@Id
+
+	@Column(name = "id_estudiante")
 	private int nro_libreta;
 
-	@Column
-	private int carrera;
-	
-	// @ManyToOne
-	// @JoinColumn(name = "id_carrera")
-	// private Inscripto inscripto;
-	
-	/*
-	
-		// private Estudiante estudiante;
-		// @JoinColumn(name = "nro_libreta")
-	 * @Id
-	 * 
-	 * @JoinColumn(name="nro_libreta")
-	 * private Estudiante estudiante;
-	 */
-	// @ManyToOne
-	// @JoinColumn(name="id_carrera")
-	// @Column
-	// private int carrera;
-	// private Carrera carrera;
+	@ManyToOne
+	@JoinColumn(name = "ID_Departamento")
+	private Carrera carrera;
 
 	@Column
 	private int antiguedad;
@@ -38,20 +20,12 @@ public class Inscripto {
 	@Column
 	private boolean graduado;
 
-	/// @JoinColumn(name="")
-
-	// @ManyToOne
-	// private Lis<Estudiante> estudiante;
-
-	// @ManyToOne
-	// private Lis<Carrera> carreras;
-
 	public Inscripto() {
 		super();
 	}
 
-	public Inscripto(int nro_libreta, int carrera, int antiguedad, boolean graduado) {
-		this.nro_libreta =nro_libreta ;
+	public Inscripto(int nro_libreta, Carrera carrera, int antiguedad, boolean graduado) {
+		this.nro_libreta = nro_libreta;
 		this.carrera = carrera;
 		this.antiguedad = antiguedad;
 		this.graduado = graduado;
@@ -68,13 +42,12 @@ public class Inscripto {
 	public void setGraduado(boolean graduado) {
 		this.graduado = graduado;
 	}
-	/*
-	 * @Override
-	 * public String toString() {
-	 * return "Inscripto [nro_libreta_estudiante=" + nro_libreta_estudiante +
-	 * ", id_carrera=" + id_carrera
-	 * + ", antiguedad=" + antiguedad + ", graduado=" + graduado + "]";
-	 * }
-	 */
+
+	@Override
+	public String toString() {
+		return "Inscripto [nro_libreta_estudiante=" + nro_libreta +
+				", id_carrera=" + carrera
+				+ ", antiguedad=" + antiguedad + ", graduado=" + graduado + "]";
+	}
 
 }
